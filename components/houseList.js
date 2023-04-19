7c48e2db97a44b4f2742a7ccbffb1d4921a95f4d
Ken Barnett <kbarnett7@gmail.com>
2023-04-19 13:18:42 -0700

    Added add house post call.

--- components/houseList.js
+++ components/houseList.js
@@ -4,6 +4,25 @@ import HouseRow, { HouseRowMem } from "./houseRow";
 const HouseList = () => {
     const [houses, setHouses] = useState([]);
 
+    // Example POST method implementation:
+    async function postData(url = "", data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
+
     useEffect(() => {
         const fetchHouses = async () => {
             const response = await fetch("/api/houses");
@@ -14,14 +33,22 @@ const HouseList = () => {
     }, []);
 
     const addHouse = () => {
+        const newHouse = {
+            id: 6,
+            address: "32 Valley Way, New York",
+            country: "USA",
+            price: 1000000
+        };
+
+        const insertHouse = async () => {
+            const result = postData("/api/houses", newHouse);
+        };
+
+        insertHouse();
+
         setHouses([
             ...houses,
-            {
-                id: 3,
-                address: "32 Valley Way, New York",
-                country: "USA",
-                price: 1000000
-            }
+            newHouse
         ]);
     };
 

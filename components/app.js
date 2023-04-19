cf81a28683bf575d0494dc7fc433ca801c7dbc53
Ken Barnett <kbarnett@stanfieldsystems.com>
2023-04-19 08:09:01 -0700

    Using children prop.

--- components/app.js
+++ components/app.js
@@ -1,9 +1,12 @@
 import Banner from "./banner";
 
 const App = () => {
+    const jsx = <div>Hi!</div>;
+
     return (
         <div>
-            <Banner headerText="Providing houses all over the world." />
+            {jsx}
+            <Banner><div>Providing houses all over the world.</div></Banner>
         </div>
     );
 };

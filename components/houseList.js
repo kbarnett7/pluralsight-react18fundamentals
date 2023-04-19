4f3bc1eb2769ef8902030d2987e9052330a4410d
Ken Barnett <kbarnett7@gmail.com>
2023-04-19 12:18:48 -0700

    Added state.

--- components/houseList.js
+++ components/houseList.js
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import HouseRow from "./houseRow";
 
-const houses = [
+const houseArray = [
     {
         id: 1,
         address: "12 Valley of Kings, Geneva",
@@ -16,6 +17,20 @@ const houses = [
 ];
 
 const HouseList = () => {
+    const [houses, setHouses] = useState(houseArray);
+
+    const addHouse = () => {
+        setHouses([
+            ...houses,
+            {
+                id: 3,
+                address: "32 Valley Way, New York",
+                country: "USA",
+                price: 1000000
+            }
+        ]);
+    };
+
     return (
         <>
             <div className="row mb-2">
@@ -35,6 +50,9 @@ const HouseList = () => {
                     {houses.map(h => <HouseRow key={h.id} house={h} />)}
                 </tbody>
             </table>
+            <button className="btn btn-primary" onClick={addHouse}>
+                Add
+            </button>
         </>
     );
 };
